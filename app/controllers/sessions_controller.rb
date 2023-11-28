e64faae2c24d4991407bbe523f9571ea1cdaa446
Luis <lvas248@gmail.com>
2023-11-28 16:40:13 -0500

    Frontend: add refresh and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,14 @@ class SessionsController < ApplicationController
 
     def create #login
         user = User.find_by(email: user_params[:email])
+
         if(user&.authenticate(user_params[:password]))
             session[:user_id] = user.id
             render json: user, status: :ok
         else 
             render json: { error: 'Invalid email or password'}, status: :unauthorized
         end
+        
     end
 
     def destroy #logout
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import { useState, React, createContext } from 'react';
+import { useState, React, createContext, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './Layout/Navbar';
 import Home from './Pages/Home';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Landing from './Pages/Landing';
-import Header from './Layout/Header';
+// import Header from './Layout/Header';
 
 export const UserContext = createContext()
 
@@ -13,6 +13,19 @@ function App() {
 
   const [ user, setUser ] = useState({ loggedIn: false })
 
+  useEffect(()=>{
+    fetch('/me').then(res => {
+      if(res.ok) res.json().then(data => setUser({...data, loggedIn: true}))
+    })
+  },[])
+
+  function logout(){
+    fetch('/logout',{
+      method: 'DELETE'
+    }).then(res => {
+        if(res.ok) setUser({loggedIn: false})} 
+      )
+  }
 
   return (
 
@@ -20,7 +33,8 @@ function App() {
 
       <div className="App">
 
-        <Header />
+        <button onClick={logout} className={`${ !user.loggedIn && 'hidden'} absolute top-2 ml-2 text-xs `}>logout</button>
+
 
         <Routes>
 
