7fb7825eeab19099898b160f7afe650995c41d81
Luis <lvas248@gmail.com>
2023-12-08 11:00:04 -0500

    Frontend: update color change issue in gameboard

--- client/src/Pages/Game/Row.js
+++ client/src/Pages/Game/Row.js
@@ -7,13 +7,20 @@ function Row({i, gameBoard, tile, row}) {
         const { char, correct, exists } = gameBoard[i][n]
 
         const bgColor = correct ? '#6aa964' : ( exists ? '#c9b458' : '#787c7e' )
+        
+        console.log(i < row)
         renderInputs.push( 
             <div key={n} id={i.toString()+ n.toString()} 
                 className={
-                    `                    
+                    `       
                     tile 
-                    ${ i < row && `bg-[${bgColor}] text-white border-none`}
-                    `} >
+                    ${ correct && 'bg-[#6aa964]'}
+                    ${ (!correct && exists) && 'bg-[#c9b458] '}
+                    ${(!correct && !exists && i<row )  && 'bg-[#787c7e]'} 
+                    ${ i < row && 'text-white border-none'}
+
+                    `}>
+
                     {char}
             </div>
         )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes:{
+        glideUp: {
+          '0%': { transform: 'translateY(100%)', opacity: 0 },
+          '100%': { transform: 'translateY(0%)', opacity: 1 },
+        },
+      },
+      animation:{
+        'glide-up':'glideUp 800ms ease-out ',
 
+      }
     },
   },
   plugins: [],
